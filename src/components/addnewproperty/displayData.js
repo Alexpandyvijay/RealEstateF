65ab2220165dd2d29e13453874417d4e33fc016b
alexpandy <alexpandyvijay@gmail.com>
2022-10-20 18:17:07 +0530

    new feature pagination

--- src/components/addnewproperty/displayData.js
+++ src/components/addnewproperty/displayData.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useContext} from "react";
+import React, {useState,useEffect,useContext, useMemo} from "react";
 import imgcon from "./images/Path 244.png";
 import eyecon from "./images/Path 71.png";
 import pen from "./images/Path 225.png";
@@ -11,8 +11,10 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function UserList(props){
-    const [userInfo, setUserInfo] = useState([])
-    const [searchApiData, setSearchApiData] = useState([])
+    const [userInfo, setUserInfo] = useState([]);
+    const [page,setPage] = useState([]);
+    const [pageCount,setPageCount] = useState([]);
+    const [searchApiData, setSearchApiData] = useState([]);
     const [filter, setFilter] = useState('');
     const [useInfo] = useContext(userContext);
     const navigate = useNavigate();
@@ -26,27 +28,88 @@ export default function UserList(props){
           }})).json();
         if(data.status==="success"){
           setUserInfo(data.docs);
-          setSearchApiData(data.docs)
+          setSearchApiData(data.docs);
         }else{
-          navigate("/")
+          navigate("/");
         }
       }
       fetchdata();
-    },[navigate,useInfo.accessToken])
+    },[navigate,useInfo.accessToken]);
+
+    useMemo(()=>{
+      let arr = [];
+      let totalPage = Math.ceil(userInfo.length/6);
+      if(totalPage>2){
+        setPageCount([1,2]);
+      }else{
+        for(let i=1;i<=totalPage;i++){
+          arr[i]=i;
+        }
+        setPageCount(arr);
+      }
+    },[userInfo]);
+
+    const currentPage=(e)=>{
+      let el = e.target.innerText;
+      el = parseInt(el);
+      let end = el*6;
+      let start = end-6;
+      if(end >= userInfo.length){
+        end = userInfo.length;
+        start = (el-1)*6;
+      }
+      if(end === start){
+        setPage(userInfo.slice(start));
+      }else{
+        setPage(userInfo.slice(start,end));
+      }
+    }
     
     const handleFilter = (e) => {
       if (e.target.value === '') {
-        setUserInfo(searchApiData)
+        setUserInfo(searchApiData);
       } else {
         const filterResult = searchApiData.filter((item) => item.PPDID.toLowerCase().includes(e.target.value.toLowerCase()))
-        setUserInfo(filterResult)
+        setUserInfo(filterResult);
       }
       setFilter(e.target.value)
     }
 
+    useMemo(()=>{
+      if(userInfo.length<=6){
+        setPage(userInfo)
+      }else{
+        setPage(userInfo.slice(0,6));
+      }
+    },[userInfo]);
+
+    const handlePageCountInc = ()=>{
+      let totalPage = Math.ceil(userInfo.length/6);
+      if(totalPage > 2){
+        if((pageCount[1]+2)<=totalPage){
+          let newPage = pageCount.map((el)=>(el+2));
+          setPageCount(newPage);
+        }else{
+          let newPage=[];
+          for(let j=pageCount[1]+1;j<=totalPage;j++){
+            newPage.push(j);
+          }
+          setPageCount(newPage);
+        }
+      }
+    }
+
+    const handlePageCountDec= ()=>{
+      if((pageCount[0]-2)>0){
+        let newPage=[];
+        for(let k=pageCount[0]-2;k<pageCount[0];k++){
+          newPage.push(k);
+        }
+        setPageCount(newPage);
+      }
+    }
+
     const TableDetail = (props) => {
-        const Views = Math.floor(Math.random()*100);
-        const day = Math.floor(Math.random()*100);
         return (
             <div className="table-details">
                 <div>{props.inf.PPDID}</div>
@@ -54,9 +117,9 @@ export default function UserList(props){
                 <div>{props.inf.propType}</div>
                 <div>{props.inf.mobile}</div>
                 <div>{props.inf.area}</div>
-                <div>{Views}</div>
+                <div>{0}</div>
                 <div><span>{props.inf.salesType}</span></div>
-                <div>{day}</div>
+                <div>{0}</div>
                 <div><img src={eyecon} alt="eyecon"></img><img src={pen} alt="pen"></img></div>
             </div>
         );
@@ -70,18 +133,25 @@ export default function UserList(props){
             </div>
             <div className="display-content">
                 <div className="table-header">
-                    <div>PPD ID</div>
-                    <div>Image</div>
-                    <div>Property</div>
-                    <div>Contact</div>
-                    <div>Area</div>
-                    <div>Views</div>
-                    <div>Status</div>
-                    <div>Days left</div>
-                    <div>Action</div>
+                  <div>PPD ID</div>
+                  <div>Image</div>
+                  <div>Property</div>
+                  <div>Contact</div>
+                  <div>Area</div>
+                  <div>Views</div>
+                  <div>Status</div>
+                  <div>Days left</div>
+                  <div>Action</div>
                 </div>
                 <div className="align-table">
-                    {userInfo.map((info)=>{return <TableDetail key={count++} inf={info}/>})}
+                    {page.map((info)=>{return <TableDetail key={count++} inf={info}/>})}
+                </div>
+                <div className="last">
+                  <div className="pagination">
+                    <div onClick={handlePageCountDec}>&laquo;</div>
+                      {pageCount.map((el)=>{return <div key={el} onClick={(e)=>{currentPage(e)}}>{el}</div>})}
+                    <div onClick={handlePageCountInc}>&raquo;</div>
+                  </div>
                 </div>
             </div>
         </div>
